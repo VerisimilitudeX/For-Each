989c98b486dea6c49c3a71a8e4684af807d2ae8d
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-21 12:44:43 -0700

    Create Favorite Food.py

---
+++ Favorite Food.py
@@ -0,0 +1,7 @@
+import random
+fav =  ["burger", "pizza", "burrito"] 
+choice = random.randint(0, len(fav) - 1)
+print("Do you like " + fav[choice] + "?")
+answer = input("y/n\n")
+if answer == "y":
+    print("You have good taste! ")
