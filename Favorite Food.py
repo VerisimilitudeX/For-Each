da46c831c00b95c6c8593edf9f09cb68e1022864
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-21 12:45:10 -0700

    Update Favorite Food.py

--- Favorite Food.py
+++ Favorite Food.py
@@ -1,7 +1,10 @@
 import random
+
 fav =  ["burger", "pizza", "burrito"] 
 choice = random.randint(0, len(fav) - 1)
+
 print("Do you like " + fav[choice] + "?")
 answer = input("y/n\n")
+
 if answer == "y":
     print("You have good taste! ")
