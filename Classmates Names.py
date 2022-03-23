ef230364917c95e79b78db639e749a77ad25cb06
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-23 14:50:21 -0700

    Create Classmates Names.py

---
+++ Classmates Names.py
@@ -0,0 +1,9 @@
+names = ["Judy", "Martha", "Marcel", "David", "Andrew", "Kyla", "Si-Khoa", "Bruce"]
+
+# Intro
+print("Names that start with M: ")
+
+#Print names starting with M
+for person in names:
+    if person[0] == "M":
+        print(person)
